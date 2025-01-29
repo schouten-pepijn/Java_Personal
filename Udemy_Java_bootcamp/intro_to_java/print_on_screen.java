9046d69d28d1044507805944d7013a97b96228f9
Pepijn Schouten <pepijn.sch@gmail.com>
2025-01-29 11:23:21 +0100

    -

---
+++ Udemy_Java_bootcamp/intro_to_java/print_on_screen.java
@@ -0,0 +1,8 @@
+public class print_on_screen {
+    public static void main(String[] args) {
+        // print on new line
+        System.out.println("Hello, World!");
+        // print on same line
+        System.out.print("Hello, World!");
+    }
+}
---
+++ Udemy_Java_bootcamp/intro_to_java/sum_class.java
@@ -0,0 +1,10 @@
+/* Simple sum class */
+public class sum_class {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 25;
+        int z = x + y;
+
+        System.out.println("Sum of x + y = " + z);
+    }
+}
