0c027ee3dc2fea75ad0986532ae9e1c09253703e
Pepijn Schouten <pepijn.sch@gmail.com>
2025-01-30 11:12:11 +0100

    updated mortgage_calculator

--- varia/classes/mortgage_calculator.java
+++ varia/classes/mortgage_calculator.java
@@ -5,30 +5,39 @@ import java.text.NumberFormat;
 public class mortgage_calculator {
     public static void main(String[] args) {
 
-        // Instanciate global variables
+        // Instanciate global variables and scanner object
         final byte MONTHS_IN_YEARS = 12;
         final byte PERCENT = 100;
-
         Scanner scanner = new Scanner(System.in);
 
+        // Welcome message
         System.out.println("Welcome to the mortgage calculator!");
 
+        // Ask and store pricipal
         System.out.print("Principal: ");
         int principal = scanner.nextInt();
 
+        // Ask and store interest rate
         System.out.print("Annual Interest Rate: ");
         float annualInterest = scanner.nextFloat();
         float monthlyInterest = annualInterest / MONTHS_IN_YEARS / PERCENT;
 
+        // Ask ad store period
         System.out.print("Period (Years): ");
-        byte years = scanner.nextInt();
+        byte years = scanner.nextByte();
         int numberOfPayments = years * MONTHS_IN_YEARS;
 
+        // Close scanner
+        scanner.close();
+
+        // Calculate the mortgage
         double mortgage = principal
                     * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                     / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
         
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(numberOfPayments)
+        // Format the mortgage
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
 
 
 
