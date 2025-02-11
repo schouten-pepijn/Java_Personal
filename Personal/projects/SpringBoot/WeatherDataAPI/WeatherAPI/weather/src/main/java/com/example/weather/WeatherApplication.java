c2b182bf0772345328d152307f146a770813d128
Pepijn Schouten <pepijn.sch@gmail.com>
2025-02-11 11:44:35 +0100

    Added weatherAPI springboot

--- Personal/projects/SpringBoot/WeatherDataAPI/WeatherAPI/weather/src/main/java/com/example/weather/WeatherApplication.java
+++ Personal/projects/SpringBoot/WeatherDataAPI/WeatherAPI/weather/src/main/java/com/example/weather/WeatherApplication.java
@@ -1,12 +1,12 @@
-package com.example.todo;
+package com.example.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class WeatherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
+		SpringApplication.run(WeatherApplication.class, args);
 	}
 }
